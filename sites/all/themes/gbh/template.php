1995ecdd7df8df0e8bea890a49d50f83de3444e1
Mike Garthwaite <michael@systemick.co.uk>
2014-12-13 10:22:21 +0000

    Changed default title

--- sites/all/themes/gbh/template.php
+++ sites/all/themes/gbh/template.php
@@ -54,9 +54,10 @@ function gbh_preprocess_maintenance_page(&$variables, $hook) {
  * @param $hook
  *   The name of the template being rendered ("html" in this case.)
  */
-/* -- Delete this line if you want to use this function
 function gbh_preprocess_html(&$variables, $hook) {
-  $variables['sample_variable'] = t('Lorem ipsum.');
+  if (drupal_is_front_page()) {
+    $variables['head_title'] = 'Landscape Garden Design Wakefield | Gardens By Harry';
+  }
 
   // The body tag's classes are controlled by the $classes_array variable. To
   // remove a class from $classes_array, use array_diff().
